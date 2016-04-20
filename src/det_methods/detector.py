63dff89ba5d10b59926369131b9e71c854a2cb36
jd <jd@localhost.localdomain>
2016-04-20 11:04:45 +0200

    Fix for CCCHCommon packet

--- src/det_methods/detector.py
+++ src/det_methods/detector.py
@@ -40,7 +40,9 @@ class Detector:
 if __name__ == '__main__':
         #parser = Parser(4729)
         #parser.listen()
-        p = GSMTap("02 04 01 00 03 f9 e1 00 00 06 07 ba 02 00 08 00 2d 06 3f 10 0e 83 f9 7e 54 48 01 00 c6 94 aa 34 2b 2b 2b 2b 2b 2b 2b".replace(' ', '').decode('hex'))
+        im_as_packet_dump = "02 04 01 00 03 f9 e2 00 00 1e 45 1d 02 00 04 00 2d 06 3f 10 0e 83 f9 7a c0 c5 02 00 c6 94 e0 a4 2b 2b 2b 2b 2b 2b 2b"
+        #p = GSMTap("02 04 01 00 03 f9 e1 00 00 06 07 ba 02 00 08 00 2d 06 3f 10 0e 83 f9 7e 54 48 01 00 c6 94 aa 34 2b 2b 2b 2b 2b 2b 2b".replace(' ', '').decode('hex'))
+        p = GSMTap(im_as_packet_dump.replace(' ', '').decode('hex'))
         print p.version
         print p.header_length
         print p.payload_type
@@ -52,4 +54,9 @@ if __name__ == '__main__':
         print p.channel_type
         print p.antenna_number
         print p.sub_slot
+        print p.payload.message_type
+        print hexdump(p)
         print type(p.payload)
+        print hexdump(p.payload)
+        #print p.payload.encode('hex')
+        print(type(p.payload.payload))
