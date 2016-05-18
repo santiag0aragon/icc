a7c7594227e2d5205edb72e5a4e6b3ddbee0833e
Santiago Aragón <santiago.aragon@datata.mx>
2016-05-18 15:38:23 +0200

    detector condition deleted

--- icc/cell_reselection_offset.py
+++ icc/cell_reselection_offset.py
@@ -10,7 +10,7 @@ class CellReselectionOffsetDetector(Detector):
         if p.channel_type == 1 and p.payload.message_type == 0x1b:
             sys_info3 = p.payload.payload
             if sys_info3.selection_parameters_present == 1:
-                if self.s_rank < 1 and 0 < sys_info3.cell_reselection_offset <= cell_reselection_offset_threshold:
+                if sys_info3.cell_reselection_offset <= cell_reselection_offset_threshold:
                     self.update_s_rank(Detector.UNKNOWN)
-                elif self.s_rank < 2 and sys_info3.cell_reselection_offset > cell_reselection_offset_threshold:
+                elif sys_info3.cell_reselection_offset > cell_reselection_offset_threshold:
                     self.update_s_rank(Detector.SUSPICIOUS)
