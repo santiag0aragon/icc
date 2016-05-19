5edf44c900163fc881da3b9af97dab569b53550d
David Stritzl <david.stritzl@gmail.com>
2016-05-19 16:24:37 +0200

    Fix dB capitalization

--- icc/cell_reselection_offset.py
+++ icc/cell_reselection_offset.py
@@ -14,10 +14,10 @@ class CellReselectionOffsetDetector(Detector):
                 cell_reselection_offset = sys_info3.cell_reselection_offset * 2
                 if cell_reselection_offset <= cell_reselection_offset_lower_threshold:
                     self.update_s_rank(Detector.NOT_SUSPICIOUS)
-                    self.comment = "low (%d db) cell reselection offset detected"
+                    self.comment = "low (%d dB) cell reselection offset detected" % cell_reselection_offset
                 elif cell_reselection_offset <= cell_reselection_offset_upper_threshold:
                     self.update_s_rank(Detector.UNKNOWN)
-                    self.comment = "medium (%d db) cell reselection offset detected"
+                    self.comment = "medium (%d dB) cell reselection offset detected" % cell_reselection_offset
                 else:
                     self.update_s_rank(Detector.SUSPICIOUS)
-                    self.comment = "high (%d db) cell reselection offset detected"
+                    self.comment = "high (%d dB) cell reselection offset detected" % cell_reselection_offset
