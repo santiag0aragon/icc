75773798acae789cc48eda760e06d3118c7d3cbe
Iwan Timmer <irtimmer@gmail.com>
2016-05-30 21:30:29 +0200

    Cleanup messages from tic

--- icc/cellinfochecks/tic.py
+++ icc/cellinfochecks/tic.py
@@ -29,7 +29,7 @@ def tic(found_list, current_lat=52.2311057, current_lon=6.8553815, range_multipl
         for info in sorted(found_list):
             ## checking database information
             rank = 0
-            comment = ""
+            comment = None
             #print info
             if verbose:
                 print info.get_verbose_info()
@@ -38,13 +38,9 @@ def tic(found_list, current_lat=52.2311057, current_lon=6.8553815, range_multipl
                 tower = towers[0]
                 distance = calc_distance(tower.lat, tower.lon, current_lat, current_lon)
                 if distance > (tower.range * range_multiplier):
-                    print("Cell tower found in database, but in wrong location %d m (range %d m)"%(distance, tower.range))
                     comment = "Cell tower found in database, but in wrong location %d m (range %d m)"%(distance, tower.range)
                     rank = 1
-                else:
-                    print(" Cell tower found in database")
             else:
-                print("No match found in database")
                 comment =" No match found in database"
                 rank = 1
 
